9b636fd6ff14f2914e3d506298061b3866bd7763
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-16 17:14:20 -0300

    Update install.sh

--- Commons/HomePage/install.sh
+++ Commons/HomePage/install.sh
@@ -1,16 +1,6 @@
-#echo "https://guides.dataverse.org/en/latest/installation/config.html?highlight=apitermsofuse"
-
-export PAYARA=/usr/local/payara5/glassfish
-export SERVER_URL=http://localhost:8080
-export APP=/usr/local/payara5/glassfish/domains/domain1/applications/dataverse-5.12.1/
-
-export URL=https://cedapdados.ufrgs.br/
-export HOME=welcome.xhtml
-
-cp $HOME $APP/$HOME
-mkdir $APP/assets/
-mkdir $APP/assets/img/
-cp *.png $APP/assets/img/$IMG
-cp *.jpg $APP/assets/img/$IMG
-
-curl -X PUT -d $APP$HOME $SERVER_URL/api/admin/settings/:HomePageCustomizationFile
+mkdir /var/
+mkdir /var/www/
+mkdir /var/www/dataverse/
+mkdir /var/www/dataverse/branding/
+cp style.css /var/www/dataverse/branding/
+curl -X PUT -d '/var/www/dataverse/branding/welcome.xhtml' http://localhost:8080/api/admin/settings/:HomePageCustomizationFile
