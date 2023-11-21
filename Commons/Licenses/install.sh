8d4f7c341df25ac79cd8c03aba6a4242951969a3
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-21 14:28:56 -0300

    Update install.sh

--- Commons/Licenses/install.sh
+++ Commons/Licenses/install.sh
@@ -1,8 +1,7 @@
 echo '************************** Set variables'
 export API_TOKEN=fb280ada-ca6e-49df-a287-df71e621f256
-export SERVER_URL=https:/cedapdados.ufrgs.br/
+export SERVER_URL=https:/localhost:8080/
 export FILE=@files/licenseCC-BY-ND-4.0.json
 echo '************************** Send configurations'
-#curl -X POST -H 'Content-Type: application/json' -H X-Dataverse-key:fb280ada-ca6e-49df-a287-df71e621f256 --data-binary $FILE https://cedapdados.ufrgs.br//api/licenses
-curl -X POST -H 'Content-Type: application/json' --data-binary $FILE http://localhost:8080/api/licenses
+curl -X POST -H 'Content-Type: application/json' -H X-Dataverse-key:$API_TOKEN --data-binary $FILE https://cedapdados.ufrgs.br//api/licenses
 echo '************************** End proccess'
