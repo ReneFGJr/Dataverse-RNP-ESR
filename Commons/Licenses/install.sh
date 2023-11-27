4f7a217dae27c41aea250cae671455598aa172c9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-27 18:50:19 -0300

    Update install.sh

--- Commons/Licenses/install.sh
+++ Commons/Licenses/install.sh
@@ -4,5 +4,6 @@ export SERVER_URL=https:/localhost:8080/
 export FILE=@files/licenseCC-BY-ND-4.0.json
 echo '************************** Send configurations'
 echo 'Dataverse'+$APITOKEN
-curl -X POST -H 'Content-Type: application/json' -H 'X-Dataverse-key:c86d0532-c3e5-49f3-becc-de2bc769d99a' --data-binary $FILE https://localhost:8080/api/licenses
+echo 'Licenca:'+$FILE
+curl -X POST -H 'Content-Type: application/json' -H 'X-Dataverse-key:c86d0532-c3e5-49f3-becc-de2bc769d99a' --data-binary $FILE http://localhost:8080/api/licenses
 echo '************************** End proccess'
