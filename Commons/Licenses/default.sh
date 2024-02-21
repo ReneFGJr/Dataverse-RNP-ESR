df109156683f4fbd765a584ede20d8621c805767
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-02-21 07:45:06 -0300

    Update default.sh

--- Commons/Licenses/default.sh
+++ Commons/Licenses/default.sh
@@ -1,5 +1,4 @@
 #export API_TOKEN=fb280ada-ca6e-49df-a287-df71e621f256
-#export SERVER_URL=https://cedapdados.ufrgs.br/
 #export STATE=true
 #export ID=1
 curl -X PUT -H 'Content-Type: application/json' -H X-Dataverse-key:fb280ada-ca6e-49df-a287-df71e621f256 https://localhost:8080/api/licenses/default/1
